27b1a4cfe61ba0bc43269777bf9f38edd8cf0ec5
qwex03 <qlecordier@gmail.com>
2024-02-06 16:22:21 +0100

    ajout de la date

--- Ex1.php
+++ Ex1.php
@@ -1,3 +1,6 @@
+<?php
+$today = date("m.d.y");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,5 +9,8 @@
 </head>
 <body>
    <h1>Mon fichier HTML</h1>
+   <?php
+      echo "<p>".$today."</p>";
+   ?>
 </body>
 </html>
